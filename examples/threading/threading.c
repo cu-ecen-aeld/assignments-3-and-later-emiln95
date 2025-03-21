58f4d84429753f92d36cd8730ad76074130e01d3
Emil Nordqvist <some@email.com>
2025-03-21 14:22:41 +0000

    Assignment 2 unit test passed.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,4 +1,5 @@
 #include "threading.h"
+#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,6 +15,24 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    int rc;
+
+    rc = usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (rc != 0){thread_func_args->thread_complete_success = false; return thread_param;}
+
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc != 0){thread_func_args->thread_complete_success = false; return thread_param;}
+    
+    rc = usleep(thread_func_args->wait_to_release_ms);
+    if (rc != 0){thread_func_args->thread_complete_success = false; return thread_param;}
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0){thread_func_args->thread_complete_success = false; return thread_param;}
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +47,22 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data* thread_param = malloc(sizeof(struct thread_data));
+    if (thread_param == NULL){return false;}
+    
+    thread_param->mutex=mutex;
+    thread_param->wait_to_obtain_ms=wait_to_obtain_ms;
+    thread_param->wait_to_release_ms=wait_to_release_ms;
+
+    int rc = pthread_create(thread,NULL, threadfunc, thread_param);
+    
+    if(rc != 0){
+        free(thread_param);
+        return false;
+    }
+
+
+    return true;
 }
 
