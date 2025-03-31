c263d0a3ab4a8108b25f00d4d1c5e3521c77f044
Emil Nordqvist <some@email.com>
2025-03-31 10:15:23 +0000

    updated startstop script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,14 +4,13 @@ if [ $1 = "start" ]; then
 
     echo "Starting aesdsocket"
     start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
-    ;;
 
 
 elif [ $1 = "stop" ]; then
 
     echo "Stoping aesdsocket"
     start-stop-daemon -K -n aesdsocket
-    ;;
+    
 else
 
     echo "Usage: $0 {start | stop}"
